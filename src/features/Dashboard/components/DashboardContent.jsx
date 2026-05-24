bc41ef7178cbac25cd30e54b752f83f390394533
Aleynaakduman <akdumanaleyna9@gmail.com>
2026-05-24 13:47:17 +0300

    Dashboard kartlar tasarlandı

--- src/features/Dashboard/components/DashboardContent.jsx
+++ src/features/Dashboard/components/DashboardContent.jsx
@@ -1,30 +1,50 @@
+import './DashboardContent.css'
+import { MdOutlineInventory } from "react-icons/md";
+import { BsExclamationCircle } from "react-icons/bs";
+import { IoCloseCircleOutline } from "react-icons/io5";
+
 
 const DashboardContent = () => {
   return (
     <div>
         <div className="dashboard-cards">
-            <div className="card">
-                <div className="card-content1">
-                    <p>Toplam Ürün</p>
-                    <p>128</p>
-                    <p>Tüm ürün sayısı</p>
+            <div className="card1" >
+                <div className="card-conteiner">
+                   
+                        <div className="card-icon1">
+                            <MdOutlineInventory/>
+                        </div>
+                   
+                    <div className="card-content">
+                        <p>Toplam Ürün</p>
+                    <h3>128</h3>
+                    <p className='card-text' >Tüm ürün sayısı</p>
+                    </div>
                 </div>
             </div>
 
-            <div className="card">
-                <div className="card-content2">
-                          <p>Kritik Stok</p>
-                    <p>18</p>
-                    <p>Kritik stokta olan ürünler</p>
+            <div className="card2"> 
+                <div className="card-conteiner">
+                   <div className="card-icon2" > <BsExclamationCircle/></div>
+                        <div className="card-content">
+                              <p>Kritik Stok</p>
+                    <h3>18</h3>
+                    <p  className='card-text' >Kritik stokta olan ürünler</p>
+                        </div>
                 </div>
             </div>
 
 
-            <div className="card">
-                <div className="card-content3">
-                      <p>Tükenenler</p>
-                    <p>7</p>
-                    <p>Stokta tükenen ürünler </p>
+            <div className="card3">
+                <div className="card-conteiner">
+                   <div className="card-icon3">
+                     <IoCloseCircleOutline/>
+                   </div>
+                      <div className="card-content">
+                        <p>Tükenenler</p>
+                    <h3>7</h3>
+                    <p  className='card-text' >Stokta tükenen ürünler </p>
+                      </div>
                 </div>
             </div>
         </div>
